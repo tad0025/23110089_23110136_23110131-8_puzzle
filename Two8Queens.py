1512ced01b982de72094caa7ee4944b4ad154fdd
TrinhDaiNghia <23110131@student.hcmute.edu.vn>
2025-09-01 19:10:42 +0700

    Update Two8Queens.py

--- Two8Queens.py
+++ Two8Queens.py
@@ -1,11 +1,13 @@
 import tkinter as tk
+from idlelib.configdialog import font_sample_text
 from tkinter import ttk
-
+import random
 from debugpy.common.sockets import close_socket
+from win32cryptcon import dwFORCE_KEY_PROTECTION_USER_SELECT
 
 root = tk.Tk()
 root.title("Two 8x8 Boards - 8 Queens")      # Khởi tạo cửa sổ chính
-root.geometry("1600x900")
+root.geometry("1200x770")
 root.rowconfigure(0, weight=1)     # cho phép stretch nếu cần
 root.columnconfigure(0, weight=1)
 
@@ -41,6 +43,20 @@ queen_id_right = [[None]*N for i in range(N)]
 
 queen_cols_right = [-1]*N
 
+
+queens= [-1] *N
+
+#khai báo cols để lưu cột đang bị hậu chiếm
+cols= [0]*N
+
+# khai báo cross1 để lưu đường chéo chính đang bị hậu chiếm
+
+cross1 = [0] * (2*N-1)
+
+# khai báo cross2 để lưu đường chéo phụ đang bị hậu chiếm
+cross2 = [0] * (2*N-1)
+
+
 #Vẽ lưới ô
 for r in range(N):
     for c in range(N):
@@ -73,22 +89,21 @@ for r in range(N):
     for c in range(N):
         boardA.tag_bind(f"l_{r}_{c}", "<Button-1>", lambda e, rr = r, cc= c: BoardA_Click(e, rr, cc))
 
-def draw_queens_inBoardB(canvas, queen_cols, queen_id_matr):
+def draw_queens_inBoardB(sol):
     #Xóa taất cả queen cũ
     for r in range(N):
         for c in range(N):
             if queen_id_right[r][c] is not None:
-                canvas.delete(queen_id_matr[r][c])
-                queen_id_matr[r][c] = None
+                boardB.delete(queen_id_right[r][c])
+                queen_id_right[r][c] = None
 
     #Vẽ mới
-    for r in range(N):
-        c= queen_cols[r]
+    for r,c in enumerate(sol):
         if c is not None and c >= 0:
             cx = c * TILE + TILE // 2
             cy = r * TILE + TILE // 2
-            qid = boardA.create_text(cx, cy, text="♛", font=("Arial", int(TILE * 0.8)))
-            queen_id_matr[r][c] = qid
+            qid = boardB.create_text(cx, cy, text="♛", font=("Arial", int(TILE * 0.8)))
+            queen_id_right[r][c] = qid
 
 # ví dụ format đơn giản: "0,4,7,5,2,6,1,3" (mỗi phần tử là col index cho row tương ứng)
 def parse_pos(s):
@@ -105,7 +120,6 @@ def parse_pos(s):
             cols.append(int(p))
     return cols
 # khi user bấm nút "Load", gọi parse_positions(input_text) rồi gán vào queen_cols_right, sau đó draw_queens_inBoardB(...)
-
 ctrl = ttk.Frame(main,padding = 4)
 ctrl.grid(row = 1, column= 0, columnspan=2, sticky="ew")
 
@@ -117,15 +131,54 @@ def random_preset():
     pass
 
 def clear_boards():
-    pass
+    for r in range(N):
+        for c in range(N):
+            if queen_id_right[r][c] is not None:
+                boardB.delete(queen_id_right[r][c])
+                queen_id_right[r][c] = None
+
 
 def start_solve():
-    pass
+    # RESET trạng thái trước khi tìm
+    for i in range(N):
+        queens[i] = -1
+        cols[i] = 0
+    for i in range(2*N - 1):
+        cross1[i] = 0
+        cross2[i] = 0
+    def dfs(row):
+        if row == N:
+            draw_queens_inBoardB(queens)  # vẽ lên board B
+            return True # dừng lại sau 1 nghiệm
+
+        for col in range(N):
+            d1 = row - col + (N + 1)
+            d2 = row + col
+            # nếu cột hoặc đường chéo đã bị chiếm thì bỏ qua
+            if cols[col] or cross1[d1] or cross2[d2]:
+                continue
+
+            queens[row]= col
+            cols[col] = 1
+            cross1[d1] = 1
+            cross2[d2] = 1
+
+            # đệ quy sang hàng tiếp theo; nếu tìm được nghiệm thì bubble up True để dừng
+            if dfs(row + 1):
+                return True
+
+            # backtrack: bỏ đặt
+            queens[row] = -1
+            cols[col] = 0
+            cross1[d1] = 0
+            cross2[d2] = 0
+        return False
+    dfs(0)
 
 
-btnLoad = ttk.Button(ctrl, text="Load Preset", command=lambda :load_entry())
-btnRandom = ttk.Button(ctrl, text="Random Preset", command=lambda: random_preset())
-btnClear = ttk.Button(ctrl, text="Clear Boards", command=lambda : clear_boards())
-btnStart = ttk.Button(ctrl, text="Solve", command=lambda :start_solve())
+btnClear = ttk.Button(ctrl, text="Clear board", width=50, command=lambda : clear_boards())
+btnClear.pack(expand=True, padx=6, pady=6)
+btnStart = ttk.Button(ctrl, text="Solve", width=50, command=lambda :start_solve())
+btnStart.pack(expand=True, padx=6, pady=6)
 
 root.mainloop()
